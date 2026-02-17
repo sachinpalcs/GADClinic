f1b393d93adce9510bf6cea96a3d3e1371067a2a
sachinpalcs <sachin004pal@gmail.com>
2026-02-17 14:14:47 +0530

    add scroll to top

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import { Scroll } from 'lucide-react';  
+import ScrollToTop from './ScrollToTop';
 const MainLayout = () => {
     return (
         <>
+        <ScrollToTop />
         <Navbar />
         <Outlet />
         <Footer />
